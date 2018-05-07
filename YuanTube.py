93c8c4e6006bafc196439817f9efd392c0433384
Damnsure <bing900713@163.com>
2018-05-07 17:09:00 +0800

    Test on Post and Get

--- YuanTube.py
+++ YuanTube.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
+@app.route('/login',methods=['POST','GET'])
+def login():
+    if request.method == 'POST':
+        return 'This is a post request'
+    else:
+        return 'This is a get request'
 
 @app.route('/hello/<int:user_id>')
 def hello_world(user_id):
