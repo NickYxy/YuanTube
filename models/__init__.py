f830bd5098a552a618456dede4b970ac609cb995
Damnsure <bing900713@hotmail.com>
2018-05-09 16:55:25 +0800

    添加user_untils工具文件

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,10 @@
 __author__ = 'nickyuan'
+
+
+from config import config
+#导入db配置
+db = config.db
+
+
+def timestamp():
+    return int(time.time)
---
+++ user_util/utils.py
@@ -0,0 +1,21 @@
+__author__ = 'nickyuan'
+
+# ------------------------- 通用配置 --------------------------
+import time
+import json
+from uuid import uuid4
+
+# ------------ 时间 -------------
+def time_str(t):
+    return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(int(t)+ 3600 * 8))
+
+
+def ymd_timestamp(s):
+    return int(time.mktime(time.strptime(s, '%Y-%m-%d')))
+
+
+# ------------ uuid -------------
+def short_uuid():
+    seed = str(uuid4())
+    short_seed = seed.split('-')[-1]
+    return short_seed
