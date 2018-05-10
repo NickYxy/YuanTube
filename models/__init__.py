03755b0660354739c70b8fc57db0187190f36035
Damnsure <bing900713@hotmail.com>
2018-05-10 10:47:47 +0800

    Add mongodb related lookup functions

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,32 @@
 __author__ = 'nickyuan'
 
-
+from user_util.utils import *
 from config import config
 #导入db配置
 db = config.db
 
 
-def timestamp():
-    return int(time.time)
+class ModelMixin(object):
+    pass
+
+
+def next_id(name):
+    query = {
+        'name':name
+    }
+    update = {
+        '$inc':{
+            'seq':1
+        }
+    }
+    kwargs = {
+        'query':query,
+        'update':update,
+        'upsert':True,
+        'new':True,
+    }
+    doc = db['data_id']
+    new_id = doc.find_and_modify(**kwargs).get('seq')
+    return new_id
+
+
