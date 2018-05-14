b626450253df81128b99430725cec2a89dbe1216
Damnsure <bing900713@hotmail.com>
2018-05-14 17:01:41 +0800

    404 fault

--- YuanTube.py
+++ YuanTube.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask_script import Manager, Shell, Command
-
+from flask import abort
 
 app = Flask(__name__)
 
@@ -30,10 +30,32 @@ def login():
     else:
         return 'This is a get request'
 
+
+#----------------Error Handler-----------------#
+@app.route('/error')
+def error():
+    abort(404)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('error/404.html'),404
+
+
 @app.route('/hello/<name>')
 def hello_world(name=None):
     return render_template('hello_test.html', name=name)
 
+@manager.command
+def server():
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.jinja_env.auto_reload = True
+    config = dict(
+        debug=True,
+        host='0.0.0.0',
+        port=8001,
+    )
+    app.run(**config)
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
