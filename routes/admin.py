878a58393096c6d4ffe12c4ef07a05d60c946be0
Damnsure <bing900713@hotmail.com>
2018-05-19 12:25:37 +0800

    Admin

--- routes/admin.py
+++ routes/admin.py
@@ -1,4 +1,12 @@
 __author__ = 'nickyuan'
 from flask import Blueprint
+from routes import *
+from models.user import User
 
 main = Blueprint('admin', __name__)
+
+# ------------------------- 用户管理 --------------------------
+'''
+暂时admin的功能是管理用户，包括删除、增加、修改信息、指定VIP；
+另外一层是对于视频的管理，包括删除、增加、修改。
+'''
