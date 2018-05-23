c7dc51eecf700f97ee5668da805028570da31c5a
Damnsure <bing900713@hotmail.com>
2018-05-23 22:38:55 +0800

    no message

--- models/comment.py
+++ models/comment.py
@@ -9,3 +9,27 @@ from flask import url_for
 import os
 
 # ----------------Comment----------------- #
+
+
+class Comment(MongoModel):
+    @classmethod
+    def _fields(cls):
+        fields = [
+            ('username', str, ''),
+            ('mobile', str, ''),
+            ('email', str, ''),
+            ('password', str, ''),
+            ('avatar', str, 'default.png'),
+            ('role', str, 'user'),
+            ('salt', str, 'q43129dhs*3'),
+            ('status', str, 'phone_unchecked'),
+        ]
+        fields.extend(super()._fields())
+        return fields
+
+    @classmethod
+    def new(cls, form):
+        m = super().new(form)
+        m.password = m.salted_password(form.get('password', ''))
+        m.save()
+        return m
