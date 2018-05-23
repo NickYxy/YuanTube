64086a8c35fddc83188230b3fa9533b30ae33e87
Damnsure <bing900713@hotmail.com>
2018-05-23 22:37:47 +0800

    no message

--- models/comment.py
+++ models/comment.py
@@ -1,1 +1,11 @@
 __author__ = 'nickyuan'
+from models import *
+from . import MongoModel
+from . import timestamp
+from enum import Enum
+from flask import current_app as app
+from flask import url_for
+
+import os
+
+# ----------------Comment----------------- #
