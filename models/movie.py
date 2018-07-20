f068b3f942f9212de49760750ff778e8a385556d
Damnsure <bing900713@hotmail.com>
2018-07-20 09:42:20 +0800

    添加电影分类

--- models/movie.py
+++ models/movie.py
@@ -9,6 +9,61 @@ from flask import url_for
 import os
 
 
+class MovieCategory(MongoModel):
+    @classmethod
+    def _fields(cls):
+        fields = [
+            ('name', str, ''),
+        ]
+        fields.extend(super()._fields())
+        return fields
+
+    @classmethod
+    def valid(cls, form):
+        name = form.get('name', '')
+        valid_name = cls.find_one(name=name) is None
+        msgs = []
+        if not valid_name:
+            message = '该分类已存在'
+            msgs.append(message)
+        status = valid_name
+        return status, msgs
+
+    @classmethod
+    def new(cls, form):
+        m = super().new(form)
+        return m
+
+    @classmethod
+    def count(self, uuid):
+        c = MovieCategory.get_uuid(uuid)
+        ms = Movies.all()
+        l = 0
+        # print(c)
+        for m in ms:
+            if m.category == c.name:
+                l += 1
+        return l
+
+
+class Category(Enum):
+    剧情 = 1
+    喜剧 = 2
+    动作 = 3
+    爱情 = 4
+    科幻 = 5
+    动画 = 6
+    悬疑惊悚恐怖 = 7
+    纪录片 = 8
+    音乐 = 9
+    家庭 = 10
+    儿童 = 11
+    历史战争 = 12
+    奇幻冒险 = 13
+    灾难 = 14
+    武侠 = 15
+
+
 # 评分的展示？
 
 
@@ -17,14 +72,19 @@ class Movies(MongoModel):
     def _fields(cls):
         fields = [
             ('name', str, ''),
+            ('category', str, ''),
+            ('actor', list, []),
+            ('category_uuid', str, ''),
             ('visible', str, ''),
             ('type', str, ''),
-            ('status', str, ''),
-            ('establishDate', str, ''),
+            ('status', str, 'invisible'),
+            ('establishDate', int, 0),
             ('mark', str, ''),
             ('comments', str, ''),
             ('cover', str, ''),
             ('content', str, ''),
+            ('country', str, ''),
+            ('uploadTime', int, 0),
         ]
         fields.extend(super()._fields())
         return fields
@@ -41,8 +101,9 @@ class Movies(MongoModel):
         return status, msgs
 
     @classmethod
-    def new(cls, form):
-        m = super().new(form)
+    def new(cls, form, **kwargs):
+        m = super().new(form, **kwargs)
+        m.category_uuid = MovieCategory.find_one(name=m.category).uuid
         return m
 
     def update_pic(self, pic):
@@ -57,26 +118,16 @@ class Movies(MongoModel):
             self.save()
         return self
 
-    # def qiniu_pic(self, key='default.png'):
-    #     self.pic = '{}{}{}'.format(app.config['CDN_URL'], key, '-webp')
-    #     self.save()
-
-    def set_pic_url(self, url):
-        if len(url) > 0:
-            self.pic = url
-            self.save()
-
     def pic_upload(self, pic):
         allowed_type = app.config['ALLOWED_UPLOAD_TYPE']
-        if pic.filename != '' and pic.filename.split('.')[-1] in allowed_type and len(self.pics) <= 20:
-            filename = '{}_{}.{}'.format(self.uuid, timestamp(), app.config['MOVIE_PIC_EXT'])
+        if pic is not None and pic.filename != '' and pic.filename.split('.')[-1] in allowed_type:
+            filename = '{}_{}.{}'.format(self.uuid, timestamp(), app.config['PRODUCT_PIC_EXT'])
             _file = '../' + app.config['MOVIE_PIC_DIR'] + filename
             _root = os.path.dirname(os.path.abspath(__file__))
             path = os.path.join(_root, _file)
             pic.save(path)
-            self.pics.append(filename)
+            self.pics = filename
             self.save()
-
             return url_for('static', filename='movie_pic/' + filename)
         else:
             return False
--- routes/movie.py
+++ routes/movie.py
@@ -1,8 +1,54 @@
 from . import *
-from models.movie import Movies
+from models.movie import Movies, MovieCategory
 
 main = Blueprint('movie', __name__)
 
+
+# ------------------------- 电影分类管理 --------------------------
+
+@main.route('/course/category/new', methods=['POST'])
+@admin_required
+def category_new():
+    form = request.form
+    status, msgs = MovieCategory.valid(form)
+    if status is True:
+        MovieCategory.new(form)
+        return redirect(url_for('movie.category'))
+    else:
+        flash('该分类已存在！', 'warning')
+        return redirect(url_for('movie.category'))
+
+
+@main.route('/course/category')
+@admin_required
+def category():
+    u = current_user()
+    c = MovieCategory.all()
+    return render_template('admin/movie_category.html', c=c, u=u)
+
+
+@main.route('/category/del/<uuid>')
+@admin_required
+def category_del(uuid):
+    m = MovieCategory.get_uuid(uuid)
+    m.delete()
+    return redirect(url_for('movie.category'))
+
+
+# todo 查看分类下的课程？
+@main.route('/category/<uuid>', methods=['POST'])
+@admin_required
+def category_update(uuid):
+    p = MovieCategory.find_one(uuid=uuid)
+    l = []
+    ms = Movies.all()
+    for m in ms:
+        if m.category_uuid == p.category_uuid:
+            l.append(m)
+    return redirect(url_for('movie.movies', uuid=p.uuid))
+
+
+# ------------------------- 电影管理 --------------------------
 
 @main.route('/movies')
 @admin_required
@@ -12,7 +58,7 @@ def movies():
     return render_template('admin/movie_list.html', m=m, u=u)
 
 
-@main.route('movie/add', method=['POST'])
+@main.route('movie/add', methods=['POST'])
 @admin_required
 def movie_add():
     pass
